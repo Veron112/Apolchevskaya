4f23516a0132ab1e11582ce8a71034c02663a2e4
Veron <Veron@DESKTOP-S7DAMRH>
2024-05-15 20:27:45 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Apolchevskaya.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult xxx([FromForm]int[] CheckBox, int RadioDef)
+        {
+            return View();
+        }
+    }
+}
